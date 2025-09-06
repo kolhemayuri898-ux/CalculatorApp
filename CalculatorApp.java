a01c338d32a5e7a3f58e2a668c5df000200bc61f
kolhemayuri898-ux <kolhemayuri898@gmail.com>
2025-09-06 23:21:54 +0530

    Add files via upload

---
+++ CalculatorApp.java
@@ -0,0 +1,17 @@
+package calculatorapp;
+
+public class CalculatorApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 10;
+int b = 5;
+   System.out.println("Simple Calculator");
+   System.out.println("Addition:"+ (a+b));
+   System.out.println("Subtraction:"+ (a-b));
+   System.out.println("Multiplication:"+(a*b));
+   System.out.println("Division:"+(a/b));
+   
+	}
+
+}
